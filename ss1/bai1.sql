45db36861834764778b5a2461f5c2f0fb3bcbd9c
dangphapqt1997 <dangphapqt1997@gmail.com>
2024-01-17 11:04:08 +0700

    ahhahaha

--- ss1/bai1.sql
+++ ss1/bai1.sql
@@ -1,13 +1,13 @@
-create database student_management;
+drop database if exists student_management;
+create database if not exists student_management;
 use student_management;
 create table classes(
-id_class int primary key,
+id_class int primary key auto_increment,
 name_class varchar(50)
 );
 create table teacher (
-id_teacher int,
+id_teacher int primary key auto_increment,
 name_teacher varchar(200),
 age int,
-country varchar(50),
-primary key(id_teacher)
+country varchar(50)
 );
